9cb1fbc545e70b9906b0442d398bf7361865b524
Zilas Jørgensen <zj@emd.dk>
2026-02-19 10:44:46 +0100

    Added Ionic

---
+++ src/client-app/studieplusplusgui/android/app/src/main/java/ionic/studieplusplusgui/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.studieplusplusgui;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ src/client-app/studieplusplusgui/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.studieplusplusgui',
+  appName: 'studieplusplusgui',
+  webDir: 'dist'
+};
+
+export default config;
